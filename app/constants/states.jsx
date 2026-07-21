157e1fa5ccd10779b5fa2a1ead754d62bec30ac5
Moahmed Fawzzi <mohamedfawzzimohamed@gmail.com>
2026-07-21 14:22:54 +0300

    update statesMeta to replace 'technologies' with 'production' and adjust localization strings in Arabic and English for consistency in project terminology

--- app/constants/states.jsx
+++ app/constants/states.jsx
@@ -1,6 +1,6 @@
 export const statesMeta = [
   { num: 3, key: "years" },
   { num: 40, key: "projects" },
-  { num: 15, key: "technologies" },
+  { num: 20, key: "production" },
   { num: 20, key: "frameworks" },
 ];
